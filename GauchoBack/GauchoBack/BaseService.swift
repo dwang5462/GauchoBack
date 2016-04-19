d61af92c3e8cc7718235edd962cfb7ea85ff6bc8
clholoien17 <Chilloien@gmail.com>
2016-04-18 21:38:23 -0700

    Login Page to FireBase has been made.

---
+++ GauchoBack/GauchoBack/BaseService.swift
@@ -0,0 +1,25 @@
+//
+//  BaseService.swift
+//  GauchoBack
+//
+//  Created by Carson Holoien on 4/18/16.
+//  Copyright © 2016 CS48 Group2. All rights reserved.
+//
+//https://www.youtube.com/watch?v=pHBLAWI4c1w
+//
+
+import Foundation
+import Firebase
+
+let BASE_URL = "https://gauchoback.firebaseio.com/"
+
+let FIREBASE_REF = Firebase(url: BASE_URL)
+
+var CURRENT_USER: Firebase
+{
+    let userID = NSUserDefaults.standardUserDefaults().valueForKey("uid") as! String
+    
+    let currentUser = Firebase(url: "\(FIREBASE_REF)").childByAppendingPath("users").childByAppendingPath(userID)
+    
+    return currentUser!
+}
